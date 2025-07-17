c95b1b7a2bc0c4d409cfa45500e2c823c833134e
Anamika Sawhney <anamika.sawhney22@gmail.com>
2025-07-17 15:57:58 +0530

    string stringbuilder

---
+++ MethodsDemo/StringBuilderDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDemo
+{
+    internal class StringBuilderDemo
+    {
+        static void Main()
+        {
+            string str = "a";  //imutable
+            str += "bcd";
+            str += "efg";
+            Console.WriteLine(str);
+            StringBuilder sb =new StringBuilder(); // mutable
+            sb.Append("a");
+            sb.Append("bcd");
+            sb.Append("efg");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
